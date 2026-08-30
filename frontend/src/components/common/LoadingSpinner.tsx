3d0f84e767216da5a06f9a297bc0e8aa011d7361
Rakshita <rakshita@11102006@gmail.com>
2026-08-30 22:49:00 +0530

    dummy frontend removed

--- frontend/src/components/common/LoadingSpinner.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Loader2 } from 'lucide-react';
-
-export const LoadingSpinner: React.FC = () => (
-  <div className="flex items-center justify-center p-4">
-    <Loader2 className="animate-spin text-accentBlue" size={24} />
-  </div>
-);
